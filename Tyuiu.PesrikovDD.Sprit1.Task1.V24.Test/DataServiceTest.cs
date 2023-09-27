6a52d3a65d090f3eb32fb32d21e1cc29cd33d20d
Данила Пестриков <theuselessfox1@gmail.com>
2023-09-27 11:51:43 +0300

    Task1

---
+++ Tyuiu.PesrikovDD.Sprit1.Task1.V24.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PesrikovDD.Sprit1.Task1.V24.Lib;
+
+namespace Tyuiu.PesrikovDD.Sprit1.Task1.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(0, res);
+        }
+    }
+}
---
+++ Tyuiu.PesrikovDD.Sprit1.Task1.V24/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PesrikovDD.Sprit1.Task1.V24.Lib;
+namespace Tyuiu.PesrikovDD.Sprit1.Task1.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Пестриков Д.Д | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода/вывода консольных приложений                    *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Пестриков Данила Дмитриевич  | ИИПб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычесляет результат по формуле (1-x)/(2+y) и печатает его на экране.    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double a, b;
+
+            Console.WriteLine("Введите значение переменной a:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной b:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(a, b));
+
+            Console.ReadKey();
+        }
+    }
+}
